9f937ce404fe0f174b0d321b5a8c16db69b7bb27
winnerbanjo <winnerbanjochannel@gmail.com>
2026-04-16 20:19:43 +0100

    Remove legacy studio base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/studio",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: "/studio",
-  },
   images: {
     remotePatterns: [
       {
